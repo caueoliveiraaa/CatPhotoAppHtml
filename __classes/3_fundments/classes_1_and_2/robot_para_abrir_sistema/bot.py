6d75780faa71a8e43ad7508970f7a0dac387eebd
caueoliveiraaa <leo.siscobra@gmail.com>
2023-07-01 00:09:12 -0300

    update

--- __classes/3_fundments/classes_1_and_2/robot_para_abrir_sistema/bot.py
+++ __classes/3_fundments/classes_1_and_2/robot_para_abrir_sistema/bot.py
@@ -1,6 +1,26 @@
 import pyautogui as p
 import os
 
+# documentação:
+# https://pyautogui.readthedocs.io/en/latest/screenshot.html
+
+
+def ler_dados():
+    """ Retornar dados do usuário """
+
+    # Primeiro prompt 
+    p.confirm(text='', title='', buttons=['RUN'])
+
+    # Ler dados do usuário
+    string_texto = p.prompt(
+        text='Digite qualquer text que você desejar!',
+        title='Input texto' ,
+        default=''
+    )
+
+    # Retornar texto informado pelo usuário
+    return string_texto
+
 
 def main(input_texto):
     """
@@ -11,37 +31,39 @@ def main(input_texto):
     """
 
     try:
-        # Usando hotkey para precinar 
+        # Usando hotkey para precionar 
         # teclas em sequência
         p.hotkey('win', 'r')
-        p.sleep(0.2)
+        p.sleep(0.1)
 
         # Escrever notepad via teclado
         p.typewrite('notepad')
-        p.sleep(0.2)
+        p.sleep(0.1)
 
         # Precionar enter
         p.press('enter')
-        p.sleep(0.2)
+        p.sleep(0.1)
 
         # Maximizar
         p.hotkey('win', 'up')
 
         # Clicar no centro da tela
         p.click(800, 600)
-        p.sleep(0.2)
+        p.sleep(0.1)
 
         # Transformar o input nas teclas que o robô 
         # irá precionar através do laço for
         lista_texto = list(input_texto)
 
-        for letra in lista_texto:
-            p.sleep(0.1)   
-            p.press(letra) 
+        # Digitar o texto
+        for palavra in lista_texto:
+            palavra = list(palavra)
 
-        # Mostrando mensagem de finalização do programa
-        p.sleep(2)
-        p.alert('Programa finalizado.')
+            for letra in palavra: 
+                p.press(letra)
+
+        # Prompt final
+        p.alert('Ao clicar em ok o programa notepad será finalizado!')
 
         # Usando comando taskkill (eliminar tarefa)
         # Para parar o programa notepad
@@ -51,16 +73,13 @@ def main(input_texto):
         
 
 if __name__ == '__main__':
+    # NOTE: É necessário instalar o pyautogui
+    # com o comando pip install pyautogui
+    os.system('cls')
+    
     try:
-        os.system('cls')
-        # Ler dados do usuário
-        string_texto = p.prompt(
-            text='Digite o texto',
-            title='Input texto' ,
-            default=''
-        )
-
-        # Chamar a função que roda o robô        
+        # Rodar robô
+        string_texto = ler_dados()
         main(input_texto=string_texto)
     except Exception as e:
         print(str(e))
