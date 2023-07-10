d1b39fd6e82f0890b1a27cf744ad116d50cfacac
Cauê Oliveira <77469726+caueoliveiraaa@users.noreply.github.com>
2023-07-10 14:14:00 -0300

    Update 6_order_by_group_by.sql

--- __classes/2_databases/classe_1_and_2/6_order_by_group_by.sql
+++ __classes/2_databases/classe_1_and_2/6_order_by_group_by.sql
@@ -1,21 +1,18 @@
 -- database: c:\Users\Leôncio Cauê\Desktop\apex_dir\__classes\2_databases\classe_1_and_2\mydatabase.db
 
 -- ORDER BY ordena nossos resultados se beaseando em um coluna
--- Ordenando por nome de forma crescente ASC
-
+-- Ordenando por nome de forma crescente (ASC)
 SELECT nome, email, idade
 FROM usuarios
 ORDER BY nome ASC;
 
--- Ordenando por idade de forma decrescente com DESC
-
+-- Ordenando por idade de forma decrescente com (DESC)
 SELECT nome, email, idade
 FROM usuarios
 ORDER BY idade DESC;
 
 -- Neste exemplo, a consulta seleciona os campos nome, email e idade da tabela usuarios
 -- e os ordena em ordem crescente primeiro pelo campo email e, em seguida, pelo campo nome
-
 SELECT nome, email, idade
 FROM usuarios
 ORDER BY email ASC, nome ASC;
@@ -23,9 +20,7 @@ ORDER BY email ASC, nome ASC;
 -- Neste exemplo, a consulta agrupa os usuários por idade e conta quantos usuários existem em cada grupo.
 -- O resultado será uma lista das idades distintas presentes na tabela usuarios junto com a contagem total 
 -- de usuários em cada idade.
-
-SELECT idade, COUNT(*) AS total
-FROM usuarios
+SELECT idade, COUNT(*) AS total FROM usuarios
 GROUP BY idade;
 
 -- Contando o número de usuários para cada idade presente na tabela
@@ -33,4 +28,5 @@ SELECT idade, COUNT(*) FROM usuarios
 GROUP BY idade;
 
 -- Calcular a média da idade para cada grupo de idade
-SELECT idade, AVG(idade) FROM usuarios GROUP BY idade;
+SELECT idade, AVG(idade) FROM usuarios
+GROUP BY idade;
