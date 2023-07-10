d62305bde782a2a05d0fb8883174255dfcccf961
Cauê Oliveira <77469726+caueoliveiraaa@users.noreply.github.com>
2023-07-10 15:11:39 -0300

    Update 0_create_insert.sql

--- __classes/2_databases/classe_1_and_2/0_create_insert.sql
+++ __classes/2_databases/classe_1_and_2/0_create_insert.sql
@@ -1,13 +1,6 @@
 -- database: c:\Users\Leôncio Cauê\Desktop\apex_dir\__classes\2_databases\classe_1_and_2\mydatabase.db
 
-### 40 CHARS
-### ADD, REMOVE, CHANGE COLS
-
-
-
-
 -- 1 - Criando tabelas:
-
 -- PRIMARY KEY:
 -- Auto incrementa o valor que está no campo
 -- Força o campo a não poder ter um valor null
@@ -21,15 +14,14 @@ CREATE TABLE IF NOT EXISTS usuarioss (
     -- Campo do tipo int com chave primária
     id INTEGER PRIMARY KEY,
     -- Text é usando para campos str
-    nome TEXT,
+    nome TEXT(100),
     -- INTEGER é usando para campos do tipo int
     idade INTEGER,
     -- A última coluna da tabela não precisa de vírgula 
-    email TEXT
+    email TEXT(100)
 );
 
 -- 2 - Inserindo dados em tabelas:
-
 -- INSERT INTO é o comando SQL para inserir dados em uma tabela
 INSERT INTO usuarioss (nome, idade, email)
 -- Ele é seguido pelo VALUE que armazena os valores a serem armazenados
@@ -46,14 +38,20 @@ INSERT INTO usuarioss (nome, idade, email)
 VALUES ('Brock', 36, 'bro@exemplo.com');
 
 -- 3 - Alterando tabelas:
-
--- ALTER TABLE altera uma tabela
-ALTER TABLE usuarioss
+-- ALTER TABLE altera uma tabela!
 -- RENAME TO renomeia a tabela
+ALTER TABLE usuarioss
 RENAME TO usuarios;
 
--- 4 - Resetando tabelas:
+-- ADD COLUMN adiciona uma coluna
+ALTER TABLE usuarios
+ADD COLUMN coluna_nova TEXT;
 
+-- DROP COLUMN remove uma coluna
+ALTER TABLE usuarios
+DROP COLUMN coluna_nova;
+
+-- 4 - Resetando tabelas:
 -- DELETE FROM deleta todos os rows (linhas) da tabela
 DELETE FROM usuarios;
 -- VACUUM vai resetar a contabilização do PRIMARY KEY
